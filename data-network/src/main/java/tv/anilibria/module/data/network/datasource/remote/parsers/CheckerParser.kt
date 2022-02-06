ef587f01d47112d9212a8206904e29701d29aea3
RadiationX <radiationx@yandex.ru>
2022-02-06 19:07:11 +0500

    fix updatedata

--- data-network/src/main/java/tv/anilibria/module/data/network/datasource/remote/parsers/CheckerParser.kt
+++ data-network/src/main/java/tv/anilibria/module/data/network/datasource/remote/parsers/CheckerParser.kt
@@ -1,18 +1,21 @@
 package tv.anilibria.module.data.network.datasource.remote.parsers
 
 import org.json.JSONObject
+import ru.radiationx.shared.ktx.android.mapObjects
+import ru.radiationx.shared.ktx.android.mapStrings
+import ru.radiationx.shared.ktx.android.nullString
 import tv.anilibria.module.data.network.datasource.remote.IApiUtils
 import tv.anilibria.module.data.network.datasource.remote.address.ApiAddressResponse
 import tv.anilibria.module.data.network.datasource.remote.address.ApiProxy
 import tv.anilibria.module.data.network.entity.app.updater.UpdateDataResponse
-import ru.radiationx.shared.ktx.android.nullString
+import tv.anilibria.module.data.network.entity.app.updater.UpdateLinkResponse
 import javax.inject.Inject
 
 /**
  * Created by radiationx on 27.01.18.
  */
 class CheckerParser @Inject constructor(
-        private val apiUtils: IApiUtils
+    private val apiUtils: IApiUtils
 ) {
 
     fun parseAddresses(responseJson: JSONObject): List<ApiAddressResponse> {
@@ -46,82 +49,53 @@ class CheckerParser @Inject constructor(
             }
         }
         return ApiAddressResponse(
-                addressJson.getString("tag"),
-                addressJson.nullString("name"),
-                addressJson.nullString("desc"),
-                addressJson.getString("widgetsSite"),
-                addressJson.getString("site"),
-                addressJson.getString("baseImages"),
-                addressJson.getString("base"),
-                addressJson.getString("api"),
-                ips,
-                proxies
+            addressJson.getString("tag"),
+            addressJson.nullString("name"),
+            addressJson.nullString("desc"),
+            addressJson.getString("widgetsSite"),
+            addressJson.getString("site"),
+            addressJson.getString("baseImages"),
+            addressJson.getString("base"),
+            addressJson.getString("api"),
+            ips,
+            proxies
         )
     }
 
     private fun parseProxy(proxyJson: JSONObject): ApiProxy = ApiProxy(
-            proxyJson.getString("tag"),
-            proxyJson.nullString("name"),
-            proxyJson.nullString("desc"),
-            proxyJson.getString("ip"),
-            proxyJson.getInt("port"),
-            proxyJson.nullString("user"),
-            proxyJson.nullString("password")
+        proxyJson.getString("tag"),
+        proxyJson.nullString("name"),
+        proxyJson.nullString("desc"),
+        proxyJson.getString("ip"),
+        proxyJson.getInt("port"),
+        proxyJson.nullString("user"),
+        proxyJson.nullString("password")
     )
 
     fun parse(responseJson: JSONObject): UpdateDataResponse {
-        val resData = UpdateDataResponse()
         val jsonUpdate = responseJson.getJSONObject("update")
 
-        resData.code = jsonUpdate.optInt("version_code", Int.MAX_VALUE)
-        resData.build = jsonUpdate.optInt("version_build", Int.MAX_VALUE)
-        resData.name = jsonUpdate.optString("version_name")
-        resData.date = jsonUpdate.optString("build_date")
-
-        jsonUpdate.getJSONArray("links")?.let {
-            for (i in 0 until it.length()) {
-                it.optJSONObject(i)?.let { linkJson ->
-                    resData.links.add(UpdateDataResponse.UpdateLink().apply {
-                        name = linkJson.optString("name", "Unknown")
-                        url = linkJson.optString("url", "")
-                        type = linkJson.optString("type", "site")
-                    })
-                }
-            }
+        val links = jsonUpdate.getJSONArray("links").mapObjects { linkJson ->
+            UpdateLinkResponse(
+                linkJson.getString("name"),
+                linkJson.getString("url"),
+                linkJson.getString("type")
+            )
         }
-
-        jsonUpdate.getJSONArray("important")?.let {
-            for (i in 0 until it.length()) {
-                it.optString(i, null)?.let {
-                    resData.important.add(it)
-                }
-            }
-        }
-
-        jsonUpdate.getJSONArray("added")?.let {
-            for (i in 0 until it.length()) {
-                it.optString(i, null)?.let {
-                    resData.added.add(it)
-                }
-            }
-        }
-
-        jsonUpdate.getJSONArray("fixed")?.let {
-            for (i in 0 until it.length()) {
-                it.optString(i, null)?.let {
-                    resData.fixed.add(it)
-                }
-            }
-        }
-
-        jsonUpdate.getJSONArray("changed")?.let {
-            for (i in 0 until it.length()) {
-                it.optString(i, null)?.let {
-                    resData.changed.add(it)
-                }
-            }
-        }
-
-        return resData
+        val important = jsonUpdate.getJSONArray("important").mapStrings { it }
+        val added = jsonUpdate.getJSONArray("added").mapStrings { it }
+        val fixed = jsonUpdate.getJSONArray("fixed").mapStrings { it }
+        val changed = jsonUpdate.getJSONArray("changed").mapStrings { it }
+        return UpdateDataResponse(
+            jsonUpdate.getInt("version_code"),
+            jsonUpdate.getInt("version_build"),
+            jsonUpdate.getString("version_name"),
+            jsonUpdate.getString("build_date"),
+            links,
+            important,
+            added,
+            fixed,
+            changed
+        )
     }
 }
---
+++ data-network/src/main/java/tv/anilibria/module/data/network/entity/app/updater/UpdateLinkResponse.kt
@@ -0,0 +1,7 @@
+package tv.anilibria.module.data.network.entity.app.updater
+
+data class UpdateLinkResponse(
+    val name: String,
+    val url: String,
+    val type: String,
+)
