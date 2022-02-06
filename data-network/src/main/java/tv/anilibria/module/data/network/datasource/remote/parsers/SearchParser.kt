80b3e721347dd15ec2d89eb897640895e8e93865
RadiationX <radiationx@yandex.ru>
2022-02-06 18:57:24 +0500

    fix suggestion

--- data-network/src/main/java/tv/anilibria/module/data/network/datasource/remote/parsers/SearchParser.kt
+++ data-network/src/main/java/tv/anilibria/module/data/network/datasource/remote/parsers/SearchParser.kt
@@ -1,33 +1,34 @@
 package tv.anilibria.module.data.network.datasource.remote.parsers
 
 import org.json.JSONArray
+import ru.radiationx.shared.ktx.android.nullString
 import tv.anilibria.module.data.network.datasource.remote.IApiUtils
 import tv.anilibria.module.data.network.datasource.remote.address.ApiConfig
 import tv.anilibria.module.data.network.entity.app.release.GenreItemResponse
 import tv.anilibria.module.data.network.entity.app.release.YearItemResponse
 import tv.anilibria.module.data.network.entity.app.search.SuggestionItemResponse
-import ru.radiationx.shared.ktx.android.nullString
 import javax.inject.Inject
 
 class SearchParser @Inject constructor(
-        private val apiUtils: IApiUtils,
-        private val apiConfig: ApiConfig
+    private val apiUtils: IApiUtils,
+    private val apiConfig: ApiConfig
 ) {
 
     fun fastSearch(jsonResponse: JSONArray): List<SuggestionItemResponse> {
         val result: MutableList<SuggestionItemResponse> = mutableListOf()
         for (i in 0 until jsonResponse.length()) {
             val jsonItem = jsonResponse.getJSONObject(i)
-            val item = SuggestionItemResponse()
-
-            item.id = jsonItem.getInt("id")
-            item.code = jsonItem.getString("code")
-            item.names.addAll(jsonItem.getJSONArray("names").let { names ->
+            val names = jsonItem.getJSONArray("names").let { names ->
                 (0 until names.length()).map {
                     apiUtils.escapeHtml(names.getString(it)).toString()
                 }
-            })
-            item.poster = "${apiConfig.baseImagesUrl}${jsonItem.nullString("poster")}"
+            }
+            val item = SuggestionItemResponse(
+                jsonItem.getInt("id"),
+                jsonItem.getString("code"),
+                names,
+                "${apiConfig.baseImagesUrl}${jsonItem.nullString("poster")}"
+            )
             result.add(item)
         }
         return result
