183c8f40676dc16d65707ffd2ce68274b95254de
RadiationX <radiationx@yandex.ru>
2022-02-12 18:31:56 +0500

    dereact

--- plugin/data-restapi/src/main/java/tv/anilibria/plugin/data/restapi/ApiResponseExt.kt
+++ plugin/data-restapi/src/main/java/tv/anilibria/plugin/data/restapi/ApiResponseExt.kt
@@ -1,14 +1,5 @@
 package tv.anilibria.plugin.data.restapi
 
-import io.reactivex.Single
-
-fun <T> Single<ApiResponse<T>>.handleApiResponse(): Single<T> = map {
-    val handledResponse = it.handle()
-    requireNotNull(handledResponse.data) {
-        "Data from api response can't be null"
-    }
-}
-
 fun <T> ApiResponse<T>.handleApiResponse(): T = requireNotNull(handle().data) {
     "Data from api response can't be null"
 }
