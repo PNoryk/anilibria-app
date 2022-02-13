3203a2c1a53b4928b5cfce6335e80cb530dd0f1f
RadiationX <radiationx@yandex.ru>
2022-02-13 19:22:07 +0500

    fix favs

--- data-new/src/main/java/tv/anilibria/module/data/repos/FavoriteRepository.kt
+++ data-new/src/main/java/tv/anilibria/module/data/repos/FavoriteRepository.kt
@@ -1,23 +1,31 @@
 package tv.anilibria.module.data.repos
 
+import tv.anilibria.module.data.local.ReleaseUpdateHelper
 import tv.anilibria.module.data.restapi.datasource.remote.api.FavoriteRemoteDataSource
 import tv.anilibria.module.domain.entity.Page
 import tv.anilibria.module.domain.entity.release.Release
 import javax.inject.Inject
 
 class FavoriteRepository @Inject constructor(
-    private val favoriteApi: FavoriteRemoteDataSource
+    private val favoriteApi: FavoriteRemoteDataSource,
+    private val releaseUpdateHelper: ReleaseUpdateHelper
 ) {
 
     suspend fun getFavorites(page: Int): Page<Release> {
-        return favoriteApi.getFavorites(page)
+        return favoriteApi.getFavorites(page).also {
+            releaseUpdateHelper.update(it.items)
+        }
     }
 
     suspend fun deleteFavorite(releaseId: Int): Release {
-        return favoriteApi.deleteFavorite(releaseId)
+        return favoriteApi.deleteFavorite(releaseId).also {
+            releaseUpdateHelper.update(listOf(it))
+        }
     }
 
     suspend fun addFavorite(releaseId: Int): Release {
-        return favoriteApi.addFavorite(releaseId)
+        return favoriteApi.addFavorite(releaseId).also {
+            releaseUpdateHelper.update(listOf(it))
+        }
     }
 }
