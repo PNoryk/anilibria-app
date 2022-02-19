0e93eb03c0b55520934141b1341a4aa01466303f
RadiationX <radiationx@yandex.ru>
2022-02-19 21:06:50 +0500

    release rpo

--- data/src/main/java/ru/radiationx/data/interactors/ReleaseInteractor.kt
+++ data/src/main/java/ru/radiationx/data/interactors/ReleaseInteractor.kt
@@ -1,19 +1,8 @@
 package ru.radiationx.data.interactors
 
-import android.util.Log
-import com.jakewharton.rxrelay2.PublishRelay
-import io.reactivex.Observable
-import io.reactivex.Single
-import io.reactivex.functions.BiFunction
-import ru.radiationx.shared.ktx.SchedulersProvider
 import ru.radiationx.data.datasource.holders.EpisodesCheckerHolder
 import ru.radiationx.data.datasource.holders.PreferencesHolder
-import ru.radiationx.data.entity.app.Paginated
-import ru.radiationx.data.entity.app.release.RandomRelease
 import ru.radiationx.data.entity.app.release.ReleaseFull
-import ru.radiationx.data.entity.app.release.ReleaseItem
-import ru.radiationx.data.repository.ReleaseRepository
-import java.util.concurrent.ConcurrentHashMap
 import javax.inject.Inject
 
 /**
@@ -21,12 +10,9 @@ import javax.inject.Inject
  */
 @Deprecated("old data")
 class ReleaseInteractor @Inject constructor(
-    private val releaseRepository: ReleaseRepository,
     private val episodesCheckerStorage: EpisodesCheckerHolder,
     private val preferencesHolder: PreferencesHolder,
-    private val schedulers: SchedulersProvider
 ) {
-
 
 
     /* Common */
