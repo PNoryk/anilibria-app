20acb1788474438932b0b242f0593a54dd13d6bf
RadiationX <radiationx@yandex.ru>
2022-02-23 19:40:18 +0500

    implement auth state holder

--- data-new/src/main/java/tv/anilibria/module/data/AuthStateHolder.kt
+++ data-new/src/main/java/tv/anilibria/module/data/AuthStateHolder.kt
@@ -3,10 +3,9 @@ package tv.anilibria.module.data
 import kotlinx.coroutines.flow.Flow
 import tv.anilibria.module.domain.entity.AuthState
 
-// todo implement
 interface AuthStateHolder {
 
-    suspend fun skip()
-    suspend fun get(): AuthState
     fun observe(): Flow<AuthState>
+    suspend fun get(): AuthState
+    suspend fun skip()
 }
---
+++ data-new/src/main/java/tv/anilibria/module/data/AuthStateHolderImpl.kt
@@ -0,0 +1,52 @@
+package tv.anilibria.module.data
+
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.combine
+import kotlinx.coroutines.flow.distinctUntilChanged
+import tv.anilibria.module.data.network.CookieData
+import tv.anilibria.module.data.network.CookiesStorage
+import tv.anilibria.module.domain.entity.AuthState
+import tv.anilibria.plugin.data.storage.DataStorage
+import tv.anilibria.plugin.data.storage.ObservableData
+import tv.anilibria.plugin.data.storage.StorageDataHolder
+import tv.anilibria.plugin.data.storage.storageBooleanKey
+
+class AuthStateHolderImpl(
+    private val cookiesStorage: CookiesStorage,
+    private val storage: DataStorage
+) : AuthStateHolder {
+
+    private companion object {
+        private const val SESSION_COOKIE = "PHPSESSID"
+    }
+
+    private val skippedHolder = StorageDataHolder(
+        storageBooleanKey("auth_skipped", false),
+        storage
+    )
+
+    private val skippedData = ObservableData(skippedHolder)
+
+    override fun observe(): Flow<AuthState> = cookiesStorage.observe()
+        .combine(skippedData.observe()) { cookies, isSkipped ->
+            getState(cookies, isSkipped)
+        }
+        .distinctUntilChanged()
+
+    override suspend fun get(): AuthState {
+        return getState(cookiesStorage.get(), skippedData.get())
+    }
+
+    override suspend fun skip() {
+        skippedData.put(true)
+    }
+
+    private fun getState(cookies: List<CookieData>, isSkipped: Boolean): AuthState {
+        val hasCookie = cookies.any { it.cookie.name() == SESSION_COOKIE }
+        return when {
+            hasCookie -> AuthState.AUTH
+            !hasCookie && isSkipped -> AuthState.AUTH_SKIPPED
+            else -> AuthState.NO_AUTH
+        }
+    }
+}
